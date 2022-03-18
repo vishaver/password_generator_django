2a39a586924124c715b438969e7286e454a01c01
vish2verma <vish2verma@gmail.com>
2022-03-18 15:49:40 +0530

    Initial commit

---
+++ password_generator/generator/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+# Create your views here.
+
+def home(request):
+    return render(request,'generator/home.html',{'password':'jojowerwr'})
+
+def test(request):
+    return HttpResponse('<h1>This is test message</h1>')
+
+def password(request):
+    newpass = ""
+    characters = list('abcdefghijklmnopqurstuvwxyz')
+    length1 = int(request.GET.get('length'))
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*()'))
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+    for j in range(length1):
+        newpass += random.choice(characters)
+    return render(request,'generator/password.html',{'password':newpass})
+
+def about(request):
+    return render(request,'generator/about.html')
